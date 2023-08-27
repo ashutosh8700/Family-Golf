c022fee6f6fe90d7bd60f4a1a939e8205242c76c
Ashutosh Dangi <ashutoshdangirdps@gmail.com>
2023-08-27 21:30:44 +0530

    Animation Done

--- script.js
+++ script.js
@@ -3,13 +3,32 @@ var blur = document.querySelector("#cursor-blur")
 
 // added event listener on movement of the mouse
 document.addEventListener("mousemove", function(dets){
-    crsr.style.left = dets.x+"px"
+    crsr.style.left = dets.x+ 30  +"px"
     crsr.style.top = dets.y+"px"
     blur.style.left = dets.x - 200 +"px"
     blur.style.top = dets.y - 200 +  "px"
 })
 
+// to select the all the elements 
+// select all the h4 inside the nav
+var h4all  =   document.querySelectorAll("#nav h4");
+// elem is for traverse over the individual element 
+h4all.forEach(function(elem) {
+        elem.addEventListener("mouseenter", function() {
+            crsr.style.scale = 3;
+            crsr.style.border = "1px solid #fff";
+            crsr.style.backgroundColor = "transparent"
 
+        })
+
+        elem.addEventListener("mouseleave", function() {
+            crsr.style.scale = 1;
+            crsr.style.border = "0px solid #95C11E";
+            crsr.style.backgroundColor = "#95C11E";
+
+        })
+
+})
 
 gsap.to("#nav" , {
     backgroundColor: "#000",
@@ -34,5 +53,60 @@ gsap.to("#main" , {
         start: "top -30%",
         end: "top -80%",
         scrub: 2
+    }
+})
+
+// About US Animation
+gsap.from("#about-us img, #about-us-in", {
+    y:50,
+    opacity:0,
+    duration:1,
+    stragger:0.4,
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        // markers:true,
+        start:"top 60%",
+        end:"top 58%",
+    }
+})
+
+gsap.from(".card",  {
+    scale:0.8,
+    opacity:0,
+    duration:1,
+    stragger:0.4,
+    scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        // markers:true,
+        start:"top 70%",
+        end:"top 65%",
+    }
+})
+
+gsap.from("#colon1", {
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        // markers: true,
+        start:"top 55%",
+        end:"top 47%",
+        scrub:"true"
+    }
+})
+
+gsap.from("#colon2", {
+    y:70,
+    x:70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        // markers: true,
+        start:"top 55%",
+        end:"top 47%",
+        scrub:"true"
     }
 })
